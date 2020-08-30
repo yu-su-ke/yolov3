cf7d8578cd3746f3dfc6bb88097c541e767ad0d5
yu-su-ke <mikaduki.2448@gmail.com>
2020-08-30 14:11:02 +0900

    refactoring

--- original_code/make_train_test_file.py
+++ original_code/make_train_test_file.py
@@ -10,4 +10,4 @@ for name in name_list:
     with open('../data/billboard_all_{}.txt'.format(name), 'w', encoding='utf-8') as text_file:
         for path in train_path:
             image_name = os.path.basename(path)
-            text_file.write('../billboard/images/{}\n'.format(image_name))
+            text_file.write('../billboard/images/{}/{}\n'.format(name, image_name))
