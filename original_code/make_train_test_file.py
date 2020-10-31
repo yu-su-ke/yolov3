19197fcb2bd71af502198d9e50d6fbe262c94f00
yu-su-ke <mikaduki.2448@gmail.com>
2020-11-01 02:27:49 +0900

    Refactoring

--- original_code/make_train_test_file.py
+++ original_code/make_train_test_file.py
@@ -2,12 +2,13 @@ import glob
 import os
 
 
+task = 'media'
 name_list = ['train', 'val', 'test']
 
 for name in name_list:
-    train_path = glob.glob('../../billboard/images/{}/*'.format(name))
+    train_path = glob.glob('../../{}/images/{}/*'.format(task, name))
 
     with open('../data/billboard_all_{}.txt'.format(name), 'w', encoding='utf-8') as text_file:
         for path in train_path:
             image_name = os.path.basename(path)
-            text_file.write('../billboard/images/{}/{}\n'.format(name, image_name))
+            text_file.write('../{}/images/{}/{}\n'.format(task, name, image_name))
